e780f5e37dcbc550e06cd6f5270082de5acab597
Lucian Boatright <lucian.boatright@gmail.com>
2021-08-20 12:06:57 +0100

    built up with new on hover but no transition avaliable

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,37 +3,15 @@ import logo from './img/HugoLogo_small.png'
 import emailLogo from './img/Email_logo_copy.png'
 import phoneLogo from './img/Phone_logo_copy.png'
 import navbar from "./Styling/Navbar.module.css"
-import flower from './img/largeYelow.png'
+import flower from '../ImageGallery/largeYelow.png'
 
 
 import {Link} from 'react-router-dom'
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'Item';
-        // // 1. bind your functions in the constructor.
-        // this.mouseOver = this.mouseOver.bind(this);
-        // this.mouseOut = this.mouseOut.bind(this);
-        this.state = {
-            hover: false,
-            isOpen: false,
-        };
-    }
 
-    mouseOverEmail = () => {
-        this.setState({hover: true});
-        console.log();
-    }
-    mouseOutEmail = () => {
-        this.setState({hover: false});
-    }
-    mouseOverPhone = () => {
-        this.setState({hover: true});
-        console.log();
-    }
-    mouseOutPhone = () => {
-        this.setState({hover: false});
+    state={
+        isOpen: false,
     }
 
     menuClick = () => {
@@ -43,7 +21,7 @@ class Navbar extends Component {
     }
     
 
-    render() {
+    render(){
         return (
             <nav className={navbar.navbar}>
                 <div className={navbar.logoBtn}>
@@ -53,18 +31,25 @@ class Navbar extends Component {
                     </div>
                     <div className={navbar.contactinfo}>
                         <div className={navbar.phonecontact} style={{ paddingBottom: '0.5rem' }}>
-                            <Link className={navbar.contactLink} to="/Contact" onMouseOver={this.mouseOverEmail} onMouseOut={this.mouseOutEmail}>
-                                {this.state.hover ? (<div><img src={flower} style={{ height: '1rem', display: 'inline' }}/><div style={{ display: 'inline' }} className="contactName">Info@hbgardens.co.uk</div></div>) : <div><img src={emailLogo} alt="" style={{paddingRight:'10px', display: 'inline' }} /><div style={{ display: 'inline' }} className="contactName">Info@hbgardens.co.uk</div></div>} 
+                            <div className={navbar.card}>
+                                <Link className={navbar.contactLink} to="/Contact" >
+                                {/* <img src={emailLogo} alt="" style={{paddingRight:'10px'}} /> */}
+                                {/* <div className={navbar.card}> */}
+                                    <img src={emailLogo} alt='emailLogo'/>
+                                    <img src={flower} alt='flower' className={navbar.imageOverlay}/>
+                                {/* </div> */}
+                                    <div className={navbar.contactName}>Info@hbgardens.co.uk</div>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={navbar.phonecontact}>
+                            <Link className={navbar.contactLink} to="/Contact" >
+                                <img src={phoneLogo} alt=""style={{paddingRight:'10px'}} />
+                                <div className="contactPhone">    07895785400 </div>
                             </Link>
                         </div>
-                        <div className={navbar.phonecontact} style={{ paddingBottom: '0.5rem' }}>
-                            <Link className={navbar.contactLink} to="/Contact" onMouseOver={this.mouseOverPhone} onMouseOut={this.mouseOutPhone}>
-                                {this.state.hover ? (<div><img src={flower} style={{ height: '1rem', display: 'inline' }}/><div style={{ display: 'inline' }} className="contactName">07895785400</div></div>) : <div><img src={phoneLogo} alt="" style={{paddingRight:'10px', display: 'inline' }} /><div style={{ display: 'inline' }} className="contactName">07895785400</div></div>}                             </Link>
-                        </div>
-                        {/* <div className={navbar.phonecontact}>
-                            <Link className={navbar.contactLink} to="/Contact" ><img src={phoneLogo} alt=""style={{paddingRight:'10px'}} /><div className="contactPhone">    07895785400 </div></Link>
-                        </div> */}
                     </div>
+                    
 
                     <div className={navbar.btn} onClick={this.menuClick}>
                         <div className={navbar.bar}></div>
